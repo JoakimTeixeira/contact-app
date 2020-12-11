11e915fb722c604536f856392e92faf0206acc27
Joakim Teixeira <setokim1@hotmail.com>
2020-12-11 09:40:08 -0300

    Apply eslint style-guide

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -1,55 +1,65 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Contacts = ({ contacts, handleDeleteButton, handleEditButton }) => {
-	return (
-		<article className="row">
-			<table className="table table-bordered table-striped table-responsive-sm bg-white mb-5">
-				{(() => {
-					if (contacts.length > 0) {
-						return (
-							<>
-								<thead>
-									<tr>
-										<th scope="col">First Name</th>
-										<th scope="col">Last Name</th>
-										<th scope="col">Email</th>
-										<th scope="col">Actions</th>
-									</tr>
-								</thead>
-								<tbody>
-									{contacts.map((person) => {
-										const { id, firstName, lastName, email } = person
+const Contacts = ({ contacts, handleDeleteButton, handleEditButton }) => (
+  <article className="row">
+    <table className="table table-bordered table-striped table-responsive-sm bg-white mb-5">
+      {(() => {
+        if (contacts.length > 0) {
+          return (
+            <>
+              <thead>
+                <tr>
+                  <th scope="col">First Name</th>
+                  <th scope="col">Last Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {contacts.map((person) => {
+                  const {
+                    id, firstName, lastName, email,
+                  } = person;
 
-										return (
-											<tr key={id}>
-												<td>{firstName}</td>
-												<td>{lastName}</td>
-												<td>{email}</td>
-												<td>
-													<button
-														onClick={() => handleDeleteButton(id)}
-														className="btn btn-danger m-1"
-													>
-														<i className="fas fa-trash-alt"></i>
-													</button>
-													<button
-														onClick={() => handleEditButton(id)}
-														className="btn btn-dark m-1"
-													>
-														<i className="fas fa-edit"></i>
-													</button>
-												</td>
-											</tr>
-										)
-									})}
-								</tbody>
-							</>
-						)
-					}
-				})()}
-			</table>
-		</article>
-	)
-}
+                  return (
+                    <tr key={id}>
+                      <td>{firstName}</td>
+                      <td>{lastName}</td>
+                      <td>{email}</td>
+                      <td>
+                        <button
+                          type="submit"
+                          onClick={() => handleDeleteButton(id)}
+                          className="btn btn-danger m-1"
+                        >
+                          <i className="fas fa-trash-alt" />
+                        </button>
+                        <button
+                          type="submit"
+                          onClick={() => handleEditButton(id)}
+                          className="btn btn-dark m-1"
+                        >
+                          <i className="fas fa-edit" />
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </>
+          );
+        }
+        return <div />;
+      })()}
+    </table>
+  </article>
+);
 
-export default Contacts
+export default Contacts;
+
+Contacts.propTypes = {
+  contacts: PropTypes.string.isRequired,
+  handleDeleteButton: PropTypes.string.isRequired,
+  handleEditButton: PropTypes.string.isRequired,
+};
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
-import './styles.css'
+import React from 'react';
+import './styles.css';
+import PropTypes from 'prop-types';
 
 const SearchBar = ({
-	handleSearchSubmit,
-	handleSearchBar,
-	searchBar,
-	searchQuery,
-}) => {
-	return (
-		<form className="search-bar form-inline">
-			<input
-				className="form-control mr-sm-2 collapse"
-				type="search"
-				placeholder="Search"
-				aria-label="Search"
-				ref={searchBar}
-				onChange={handleSearchBar}
-				value={searchQuery}
-			/>
-			<button
-				className="btn btn-outline-light"
-				type="submit"
-				onClick={handleSearchSubmit}
-			>
-				<i class="fas fa-search"></i>
-			</button>
-		</form>
-	)
-}
+  handleSearchSubmit,
+  handleSearchBar,
+  searchBar,
+  searchQuery,
+}) => (
+  <form className="search-bar form-inline">
+    <input
+      className="form-control mr-sm-2 collapse"
+      type="search"
+      placeholder="Search"
+      aria-label="Search"
+      ref={searchBar}
+      onChange={handleSearchBar}
+      value={searchQuery}
+    />
+    <button
+      className="btn btn-outline-light"
+      type="submit"
+      onClick={handleSearchSubmit}
+    >
+      <i className="fas fa-search" />
+    </button>
+  </form>
+);
 
-export default SearchBar
+export default SearchBar;
+
+SearchBar.propTypes = {
+  handleSearchSubmit: PropTypes.string.isRequired,
+  handleSearchBar: PropTypes.string.isRequired,
+  searchBar: PropTypes.string.isRequired,
+  searchQuery: PropTypes.string.isRequired,
+};
